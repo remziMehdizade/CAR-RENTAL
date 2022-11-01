b25f9eec7c709eec64a1a50881505403087f50f4
remziMehdizade <remzi.mehdizade@outlook.com>
2022-11-01 18:43:31 +0400

    Book A Car

--- src/order/order.resolver.ts
+++ src/order/order.resolver.ts
@@ -1,7 +1,6 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { OrderService } from './order.service';
 import { Order } from './entities/order.entity';
-import { CreateOrderInput } from './dto/create-order.input';
 @Resolver(() => Order)
 export class OrderResolver {
   constructor(private readonly orderService: OrderService) {}
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,11 +1,9 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { create } from 'domain';
-import { CarService } from 'src/car/car.service';
 import { STATUS } from 'src/car/dto/create-car.input';
 import { Car } from 'src/car/entities/car.entity';
 import { Repository } from 'typeorm';
-import { CreateOrderInput, ORDER_STATUS } from './dto/create-order.input';
+import { ORDER_STATUS } from './dto/create-order.input';
 import { Order } from './entities/order.entity';
 
 @Injectable()
@@ -14,25 +12,12 @@ export class OrderService {
     @InjectRepository(Order) private orderRepository: Repository<Order>,
     @InjectRepository(Car) private carRepository: Repository<Car>,
   ) {}
-  async create(createOrderInput: CreateOrderInput) {
-    const order = await this.orderRepository.create(createOrderInput);
-
-    const car = await this.carRepository.findOne({
-      where: { id: createOrderInput.carId },
-    });
-    if (!car) {
-      throw new NotFoundException('Car Not Found');
-    }
-    order.car = car;
-
-    return await this.orderRepository.save(order);
-  }
 
   async bookACar(id, day) {
     const order = new Order();
 
     const car = await this.carRepository.findOne({
-      where: { id },
+      where: { id, status: STATUS.AVAILABLE },
     });
     if (!car) {
       throw new NotFoundException(' Car Not Found');
